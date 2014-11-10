2a493617701d3e08be81bd613c9d9c40dc61e7ae
AstridW <AstridWcountee@gmail.com>
2014-11-10 08:16:40 -0600

    controller problems

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,19 @@
 class PinsController < ApplicationController
   def index
+    @pins = Pin.all
   end
 
   def create
+    @pin = Pin.new(params.require(:user).permit(:notes, :photo))
+    if @pin.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  def new
+    @pin - Pin.new
+  end
+
 end
